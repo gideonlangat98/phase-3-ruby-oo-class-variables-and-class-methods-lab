cf3f30cba150f843be35ca6b8ad83a1530993d63
gideonlangat98 <gideonlangat98@gmail.com>
2022-08-22 20:55:46 +0300

    commit tree e3e765cf0e9fe9f0abe36174623a4c18c5507a9e

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@genres = []
+    @@artist = []
+
+    def initialize(name, artist, genre)
+        @name=name
+        @artist=artist
+        @genre=genre
+        @@count += 1
+    end
+
+    def name=(name)
+        @name 
+    end
+
+    def artist=(artist)
+        @artist
+    end
+
+    def genre=(artist)
+        @genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def genres
+       @@genres
+    end
+
+
+
+   
+
+end
